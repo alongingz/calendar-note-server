a8d9046f2a3944107de5ff4f1fcb5e28fff1d377
aliyun20210411 <aliyun20210411@haah.com>
2021-04-11 03:18:40 +0800

    change ip

--- run_calendar_server.py
+++ run_calendar_server.py
@@ -190,4 +190,4 @@ def calendar_add(notedate):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ start-calendar-server.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("nohup python3 run_calendar_server.py 2>&1 &")
+
